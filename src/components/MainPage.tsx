c668a0d4fe682f0a8aa25a2ae5791654b7bd2b42
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-01 12:17:25 +0100

    Updated MainPage - pass props to children

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import PieGraph from './PieGraph';
+import BarGraph from './BarGraph';
 
 const MainPage = () => {
+
+  const data = [
+    {name: "Facebook", value: 2800000000},
+    {name: "Instagram", value: 1000000000},
+    {name: "Twitter", value: 330000000},
+    {name: "TikTok", value: 1000000000},
+    {name: "LinkedIn", value: 700000000},
+    {name: "SnapChat", value: 500000000},
+    {name: "Pinterest", value: 450000000},
+    {name: "Reddit", value: 430000000},
+    {name: "Telegram", value: 500000000},
+    {name: "Dicord", value: 150000000},
+    {name: "Twitch", value: 30000000},
+  ]
 
   return (
     <div>
       <h2>Social Network Users</h2>
-        <PieGraph />
+        <PieGraph data={data} />
+        <BarGraph data={data} />
     </div>
   )
 }

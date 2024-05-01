ebd710af43aac1a7290625ca1dc4635f9dd1c5c6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-01 11:10:17 +0100

    Updated MainPage.tsx

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -3,10 +3,17 @@ import { PieChart, Pie, Tooltip } from 'recharts';
 
 const MainPage = () => {
   const data = [
-    {name: "Facebook", value: 200000000},
-    {name: "Instagram", value: 150000000},
-    {name: "Twiter", value: 100000000},
-    {name: "TikTok", value: 50000000},
+    {name: "Facebook", value: 2800000000},
+    {name: "Instagram", value: 1000000000},
+    {name: "Twitter", value: 330000000},
+    {name: "TikTok", value: 1000000000},
+    {name: "LinkedIn", value: 700000000},
+    {name: "SnapChat", value: 500000000},
+    {name: "Pinterest", value: 450000000},
+    {name: "Reddit", value: 430000000},
+    {name: "Telegram", value: 500000000},
+    {name: "Dicord", value: 150000000},
+    {name: "Twitch", value: 30000000},
   ]
 
   return (

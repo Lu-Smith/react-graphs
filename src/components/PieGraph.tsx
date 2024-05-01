b8f6db2c66a5e21657a1812b1db563d2a3a196b7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-01 13:00:24 +0100

    Updated PieGraph.tsx

--- src/components/PieGraph.tsx
+++ src/components/PieGraph.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PieChart, Pie, Tooltip } from 'recharts';
+import { PieChart, Pie, Tooltip, Cell } from 'recharts';
 
 interface DataItem {
   name: string;
@@ -11,19 +11,24 @@ interface PieGraphProps {
 }
 
 const PieGraph: React.FC<PieGraphProps> = ({data}) => {
+
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF', '#FF0000', '#800000', '#00FF00', '#008080', '#000080', '#800080'];
    
   return (
     <PieChart width={400} height={400}>
         <Pie
         dataKey="value"
-        isAnimationActive={false}
+        isAnimationActive={true}
         data={data}
         cx="50%"
         cy="50%"
         outerRadius={90}
-        fill="#8884d8"
         label
-        />
+        >
+          {
+          data.map((_, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+        }
+        </Pie>
         <Tooltip />
     </PieChart>
   )

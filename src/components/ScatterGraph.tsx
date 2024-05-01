eb775074272e32c5a2443e783956df0be3d55276
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-01 13:00:03 +0100

    Created ScatterGraph.ts

---
+++ src/components/ScatterGraph.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {
+    ScatterChart,
+    Scatter,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+  } from 'recharts';
+
+  interface DataItem {
+    name: string;
+    value: number;
+    }
+
+    interface ScatterGraphProps {
+    data: DataItem[];
+    }
+
+const ScatterGraph: React.FC<ScatterGraphProps> = ({data}) => {
+  return (
+    <ScatterChart
+        width={1000}
+        height={400}
+        margin={{
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 20,
+        }}
+        >
+          <CartesianGrid />
+          <XAxis dataKey="name" name="Social Media"/>
+          <YAxis type="number" dataKey="value" name="Users"  unit="mln" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Legend />
+          <Scatter name="Social media users" data={data} fill="#8884d8" shape="star" />
+        </ScatterChart>
+  )
+}
+
+export default ScatterGraph

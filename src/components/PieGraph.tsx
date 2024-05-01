d0697e6f29fc4b15880772dfa736cf533c4c4692
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-01 12:17:37 +0100

    Updated PieGraph.tsx

--- src/components/PieGraph.tsx
+++ src/components/PieGraph.tsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import { PieChart, Pie, Tooltip } from 'recharts';
 
-const PieGraph = () => {
-    const data = [
-        {name: "Facebook", value: 2800000000},
-        {name: "Instagram", value: 1000000000},
-        {name: "Twitter", value: 330000000},
-        {name: "TikTok", value: 1000000000},
-        {name: "LinkedIn", value: 700000000},
-        {name: "SnapChat", value: 500000000},
-        {name: "Pinterest", value: 450000000},
-        {name: "Reddit", value: 430000000},
-        {name: "Telegram", value: 500000000},
-        {name: "Dicord", value: 150000000},
-        {name: "Twitch", value: 30000000},
-      ]
+interface DataItem {
+  name: string;
+  value: number;
+}
 
+interface PieGraphProps {
+  data: DataItem[];
+}
+
+const PieGraph: React.FC<PieGraphProps> = ({data}) => {
+   
   return (
     <PieChart width={400} height={400}>
         <Pie

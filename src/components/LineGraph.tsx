c4f09976544f8e5ae2beb42ac08d53b78ce58cfe
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-01 17:23:01 +0100

    Updated LineGraph.tsx

--- src/components/LineGraph.tsx
+++ src/components/LineGraph.tsx
@@ -11,14 +11,36 @@ interface LineGraphProps {
 }
 
 const LineGraph: React.FC<LineGraphProps> = ({data}) => {
+  const UNIT = 'mln'; 
+
   return (
-    <LineChart width={500} height={300} data={data}>
-    <CartesianGrid strokeDasharray="3 3" />
-    <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />
-    <YAxis />
-    <Tooltip />
-    <Legend />
-    <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
+    <LineChart width={900} height={400} data={data}>
+    <CartesianGrid strokeDasharray="2 2" />
+    <XAxis dataKey="name" padding={{ left: 30, right: 30 }} tick={{ fill: '#ff7e67' }} />
+    <YAxis tick={{ fill: '#ffe9e3' }} />
+    <Tooltip
+          content={({ payload }) => {
+            if (payload && payload.length > 0) {
+              const { name, value } = payload[0].payload;
+              return (
+                <div style={{ backgroundColor: '#393e49', padding: '1px 15px', borderRadius: '5px', border: '2px solid red' }}>
+                  <p style={{ color: '#ff7e67'}}>{`${name}:`}</p>
+                  <p>{`${value}${UNIT} users.`}</p>
+                </div>
+              );
+            }
+            return null;
+          }}
+        />
+    <Legend 
+       content={() => {
+        return (
+            <div style={{ color: '#ffe9e3' }}> {/* Change color as per your preference */}
+                Social Media Users in mln.
+            </div>
+        );
+    }}/>
+    <Line type="monotone" dataKey="value" stroke="red" activeDot={{ r: 10 }} />
   </LineChart>
   )
 }

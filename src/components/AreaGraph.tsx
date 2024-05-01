7cf4bf90643a3e72aa5e96d3f0d42e86c44ea8df
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-01 17:22:39 +0100

    Updated AreaGraph.tsx

--- src/components/AreaGraph.tsx
+++ src/components/AreaGraph.tsx
@@ -11,9 +11,11 @@ interface DataItem {
   }
 
 const AreaGraph: React.FC<AreaGraphProps> = ({data}) => {
+
+  const UNIT = 'mln'; 
   return (
     <AreaChart
-    width={500}
+    width={800}
     height={400}
     data={data}
     margin={{
@@ -23,11 +25,23 @@ const AreaGraph: React.FC<AreaGraphProps> = ({data}) => {
       bottom: 0,
     }}
     >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Area type="monotone" dataKey="value" stackId="1" stroke="#8884d8" fill="#8884d8" />
+        <CartesianGrid strokeDasharray="3 3" stroke="#f1b963"/>
+        <XAxis dataKey="name" tick={{ fill: '#ff7e67' }}/>
+        <YAxis unit="mln" tick={{ fill: '#f1b963' }}/>
+        <Tooltip 
+           content={({ payload }) => {
+            if (payload && payload.length > 0) {
+              const { name, value } = payload[0].payload;
+              return (
+                <div style={{ backgroundColor: '#dfd3c3', color: '#222831', padding: '20px 15px', margin: '0', borderRadius: '5px', width: '130px', border: '2px solid #c7b198' }}>
+                  <p style={{ color: '#c24d2c', fontWeight: 'bold', margin: '0'}}>{`${name}`}</p>
+                  <p style={{ margin: '0'}}>{`${value}${UNIT} people`}</p>
+                </div>
+              );
+            }
+            return null;
+          }}/>
+        <Area type="monotone" dataKey="value" stackId="1" stroke="#c24d2c" fill="#ff7e67" />
     </AreaChart>
   )
 }

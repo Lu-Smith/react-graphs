6471322bf4796544f1f4982c331cbf94430055b3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-01 11:00:28 +0100

    Updated MainPage.tsx

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { PieChart, Pie, Sector, Cell } from 'recharts';
 
 const MainPage = () => {
+  const data = [
+    {name: "Facebook", value: 200000000},
+    {name: "Instagram", value: 150000000},
+    {name: "Twiter", value: 100000000},
+    {name: "TikTok", value: 50000000},
+  ]
+
   return (
-    <div>MainPage</div>
+    <div>
+      <h2>Social Network Users</h2>
+    </div>
   )
 }
 

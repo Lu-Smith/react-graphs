d4733c4672c3540b03982b11c4921bad9d504b77
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-01 14:31:32 +0100

    Updated PieGraph.tsx

--- src/components/PieGraph.tsx
+++ src/components/PieGraph.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PieChart, Pie, Tooltip, Cell } from 'recharts';
+import { PieChart, Pie, Tooltip, Cell} from 'recharts';
 
 interface DataItem {
   name: string;
@@ -12,8 +12,9 @@ interface PieGraphProps {
 
 const PieGraph: React.FC<PieGraphProps> = ({data}) => {
 
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF', '#FF0000', '#800000', '#00FF00', '#008080', '#000080', '#800080'];
-   
+  const COLORS = ['#ff9a3c', '#66bfbf', '#ffb5b5', '#c7b198', '#a393eb', '#ef5a5a', '#668ba4', '#906387', '#118a7e', '#fbf2d5', '#f8da5b'];
+  const UNIT = 'mln'; 
+
   return (
     <PieChart width={400} height={400}>
         <Pie
@@ -23,13 +24,26 @@ const PieGraph: React.FC<PieGraphProps> = ({data}) => {
         cx="50%"
         cy="50%"
         outerRadius={90}
-        label
+        stroke="grey"
+        label={(entry) => entry.name}
         >
           {
           data.map((_, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
         }
         </Pie>
-        <Tooltip />
+        <Tooltip
+          content={({ payload }) => {
+            if (payload && payload.length > 0) {
+              const { name, value } = payload[0].payload;
+              return (
+                <div style={{ backgroundColor: '#000', padding: '3px 15px' }}>
+                  <p>{`${name}: ${value}${UNIT} users.`}</p>
+                </div>
+              );
+            }
+            return null;
+          }}
+        />
     </PieChart>
   )
 }

70b8508c289e15f4306bdd21d6e3bd2634958523
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-01 15:02:53 +0100

    Updated logic in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,26 @@ function App() {
   const [pageNumber, setPageNumber] = useState(1);
 
   const handlePages = () => {
-    if (pageNumber < 6) setPageNumber(prev => prev + 1);
-    else if (pageNumber === 6) setPageNumber(1);
+    if (pageNumber < 5) setPageNumber(prev => prev + 1);
+    else if (pageNumber === 5) setPageNumber(1);
   }
 
   return (
     <>
       {start ? <MainPage pageNumber={pageNumber} /> : <h1>Graphs</h1>}
       <div className="card">
-        <button onClick={() => setStart(prev => !prev)}>
-        {start ? 'Home Page' : 'Start'}
-        </button>
-        <div>
-        {start ? 
-        <div>
-          <h2>Graph {pageNumber} 📈</h2>
-          <button onClick={handlePages}>
-                  Next
+          {start ? 
+          <div>
+            <h2>Graph {pageNumber} 📈</h2>
+            <button onClick={handlePages}>
+                    Next
+            </button>
+          </div> : 
+          <h2>Discover new world of data 📈</h2>}
+          <button onClick={() => setStart(prev => !prev)}>
+            {start ? 'Home Page' : 'Start'}
           </button>
-        </div> : 
-        <h2>Discover new world of data 📈</h2>}
-        </div>
       </div>
-
     </>
   )
 }

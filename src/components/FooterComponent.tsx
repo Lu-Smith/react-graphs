64150f92cf49fe2f312be04dd27b970e19a07f6d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-02 18:26:01 +0100

    Created FooterComponent.tsx

---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <h3 className='footer'>Coded by 
+        <a href="https://www.lunasmithart.com/" target="_blank"> Luna Smith</a>, open-source on
+        <a href="https://github.com/Lu-Smith/react-graphs" target="_blank"> GitHub</a>.
+    </h3>
+  )
+}
+
+export default FooterComponent

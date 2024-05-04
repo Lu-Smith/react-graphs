db417120d7dbc16ff3e4e04721676ce0a9dd8f6d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-04 22:52:37 +0100

    Updated PieGraph.tsx

--- src/components/PieGraph.tsx
+++ src/components/PieGraph.tsx
@@ -26,7 +26,6 @@ const PieGraph: React.FC<PieGraphProps> = ({data}) => {
         cy="50%"
         outerRadius={110}
         stroke="grey"
-        labelLine={true}
         label={(entry) => entry.name}
         >
           {
